03030453b733ba0c0de612ae7b5379635931e58b
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2025-10-06 16:50:59 +0200

    Create shell01.sh

---
+++ SHELL01/shell01.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+# Jan Kwinta
+#
+# 06.10.2025
+#
+# Zadanie SHELL01
+set -e
+
+# granice przedzialu indeksow tabelki
+a=1
+b=1
+
+# sprawdzanie istnienia i sensownosci argumentow
+if (( "$#" < 1 )) ; then
+    exit 1
+elif (( "$#" == "1" )) ; then
+    b=$1
+elif (( "$1" > "$2" )) ; then
+    exit 2
+else
+    a=$1; b=$2
+fi
+
+# pierwszy wiersz tabeli - liczby od a do b
+printf "    "
+for i in $(seq $a $b) ; do
+    printf "%4d" "$i"
+done
+echo ""
+
+# pozostala czesc tabeli - indeksy + mnozenie
+for i in $(seq $a $b) ; do
+    printf "%4d" "$i"
+    for j in $(seq $a $b) ; do
+        printf "%4d" "$((i * j))"
+    done
+    echo ""
+done
