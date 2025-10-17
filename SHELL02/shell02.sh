33df068cad5d3fb50e3ba8e1cbf7016c507ed3f7
Jan Kwinta <jankwinta@icloud.com>
2025-10-17 15:20:07 +0200

    new shell02

---
+++ SHELL02/shell02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# Jan Kwinta
+#
+# 16.10.2025
+#
+# Zadanie SHELL02
+set -e
+
+while getopts "abcde" flag; do
+    case ${flag} in
+        a) echo "-a present"
+        ;;
+        b) echo "-b present"
+        ;;
+        c) echo "-c present"
+        ;;
+        d) echo "-d present"
+        ;;
+        e) echo "-e present"
+        ;;
+    esac
+done
