355b5ce0fcf9a7ca6e47ae0e58f00ce15c85d71e
KwintaJ <jankwarta@gmail.com>
2025-10-20 14:54:02 +0200

    przetwarzanie opcji, stworzenie help

---
+++ SHELL03/shell03.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+# Jan Kwinta
+#
+# 20.10.2025
+#
+# Problem SHELL03
+set -e
+
+# parsowanie opcji
+SHORT_OPTS=""
+LONG_OPTS="replace-with-hardlinks help max-depth: hash-algo:"
+
+PARSED=$(getopt --options="$SHORT_OPTS" --longoptions="$LONG_OPTS" --name "$0" -- "$@") || exit 2
+eval set -- "$PARSED"
+
+HELP=0
+MAX_DEPTH=1
+HASH_ALGO="md5"
+HARDLINKS_REPLACE=0
+
+# sprawdzenie obecnych flag opcji
+# i zaznaczenie tego w tablicy
+while true; do
+    case "$1" in
+        --help)
+            HELP=1
+            shift
+            ;;
+        --max-depth)
+            MAX_DEPTH=$2
+            shift 2
+            ;;
+        --hash-algo)            
+            HASH_ALGO=$2
+            shift 2
+            ;;
+        --replace-with-hardlinks)
+            HARDLINKS_REPLACE=1
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error: $1"
+            exit 99
+            ;;
+  esac
+done
+
+if (( $HELP == 1 )) ; then
+    echo "--------File duplicate remover--------"
+    echo ""
+    echo "    Use this script to search a directory for"
+    echo "    file duplicates and remove any if found."
+    echo ""
+    echo ""
+    echo " Usage:"
+    echo " ./shell03.sh [--replace-with-hardlinks][--max-depth=N][--hash-algo=X] DIRNAME"
+    echo ""
+    echo "    replace-with-hardlinks: Replaces duplicates with hardlinks instead of removing them."
+    echo "    max-depth: Searches in subdirectories up to N directories deep."
+    echo "    hash-algo: Uses specific hash function to compare files. Default is md5."
+    echo ""
+    echo ""
+    exit 1
+fi
