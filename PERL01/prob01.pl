666adb148bddd7989de9b187b62c33a3fa4ae277
Jan Kwinta <jankwinta@icloud.com>
2025-11-22 03:09:21 +0100

    prob01

---
+++ PERL01/prob01.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @zwierzeta = ("kot", "pies", "papuga", "kanarek", "ryba");
+
+print "$zwierzeta[0]\n";
+
+my $liczbaZwierzat = scalar @zwierzeta;
+print "$liczbaZwierzat\n";
+
+$zwierzeta[1] = "kanarek";
+
+push(@zwierzeta, "żaba");
+
+$liczbaZwierzat = scalar @zwierzeta;
+print "$liczbaZwierzat\n";
+
+pop(@zwierzeta);
+
+$liczbaZwierzat = scalar @zwierzeta;
+print "$liczbaZwierzat\n";
+
+foreach my $zwierze (@zwierzeta) {
+    print "$zwierze\n";
+}
+
+for my $i (0..@zwierzeta-1) {
+    print "$i $zwierzeta[$i]\n";
+}
+
+for my $i (1..3) {
+    print "$zwierzeta[$i]\n";
+}
