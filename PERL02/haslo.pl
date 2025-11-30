d4f3abfcf719aad1dc9b231e62fda1d33146f02f
Jan Kwinta <jankwinta@icloud.com>
2025-11-30 18:42:40 +0100

    =

---
+++ PERL02/haslo.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# Jan Kwinta
+#
+# 30.11.2025
+#
+# Problem PERL02
+# Lamacz hasel
+use strict;
+use warnings;
---
+++ PERL02/wc.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# Jan Kwinta
+#
+# 30.11.2025
+#
+# Problem PERL02
+# Word count
+use strict;
+use warnings;
