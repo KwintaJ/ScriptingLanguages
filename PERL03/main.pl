d78bee980965c9f3e107a0ec4d07b3f9f3af9558
Jan Kwinta <jankwinta@icloud.com>
2025-12-20 20:39:44 +0100

    main napisany

--- PERL03/main.pl
+++ PERL03/main.pl
@@ -9,5 +9,30 @@ use strict;
 use warnings;
 
 use lib '.'; 
+use Modul;
 
-use Modul qw();
+##########################################################
+# wartosci
+my $n;
+my $m;
+my $file_IN;
+my $file_OUT;
+
+##########################################################
+# przetworzenie argumentow wywolana
+
+if (@ARGV < 4) {
+    die "Arg error";
+}
+else {
+    $n = $ARGV[2];
+    $m = $ARGV[3];
+    $file_IN = $ARGV[0];
+    $file_OUT = $ARGV[1];
+}
+
+##########################################################
+# wykonanie funkcji z Modul
+Modul::init($n, $m);
+Modul::addReadXLS($file_IN);
+Modul::saveCSV($file_OUT);
