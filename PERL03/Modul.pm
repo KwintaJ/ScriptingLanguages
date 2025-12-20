ed57bae3175886c612a86d94b4caf36a28ba3084
Jan Kwinta <jankwinta@icloud.com>
2025-12-20 20:30:06 +0100

    perl03 init

---
+++ PERL03/Modul.pm
@@ -0,0 +1,18 @@
+#
+# Jan Kwinta
+#
+# 20.12.2025
+#
+# Problem PERL03
+# Modul.pm
+package Modul;
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+
+our @EXPORT_OK = qw();
+
+
+1;
---
+++ PERL03/main.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+# Jan Kwinta
+#
+# 20.12.2025
+#
+# Problem PERL03
+# main
+use strict;
+use warnings;
+
+use lib '.'; 
+
+use Modul qw();
