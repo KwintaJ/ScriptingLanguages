a999e63fe8955bacc62f82932c41c3281e0c3809
Jan Kwinta <jankwinta@icloud.com>
2025-12-20 20:58:41 +0100

    init + out do csv

--- PERL03/Modul.pm
+++ PERL03/Modul.pm
@@ -1,4 +1,3 @@
-#
 # Jan Kwinta
 #
 # 20.12.2025
@@ -10,23 +9,36 @@ package Modul;
 use strict;
 use warnings;
 
-use Exporter 'import';
+use Spreadsheet::ParseXLSX;
 
-our @EXPORT = qw();
+my @array;
+my ($rows, $cols);
 
 sub init {
-    my ($n, $m) = @_;
-    # TODO
+    ($rows, $cols) = @_;
+    @array = ();
+
+    for my $i (0 .. $rows - 1) {
+        for my $j (0 .. $cols - 1) {
+            $array[$i][$j] = 0;
+        }
+    }
 }
 
 sub addReadXLS {
-    my $filename = @_;
+    my ($filename) = @_;
     # TODO
 }
 
 sub saveCSV {
-    my $filename = @_;
-    # TODO
+    my ($filename) = @_;
+    open(my $fh, '>', $filename) or die "File '$filename' error $!";
+    
+    for my $row_ref (@array) {
+        print $fh join(';', @$row_ref) . "\n";
+    }
+
+    close $fh;
 }
 
 1;
