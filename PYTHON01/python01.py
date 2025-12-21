f7d6f1f3a03216009c9f22cb4e83d998c735baf4
Jan Kwinta <jankwinta@icloud.com>
2025-12-21 13:41:12 +0100

    python01

---
+++ PYTHON01/python01.py
@@ -0,0 +1,167 @@
+# Jan Kwinta
+#
+# 21.12.2025
+#
+# Problem PYTHON01
+
+import time
+import random
+import string
+import multiprocessing
+import threading
+
+##########################################################
+# wartosci stale - konfiguracja
+NUM_RECORDS = 250000
+NUM_WORKERS = 4
+
+##########################################################
+# tworzenie slownika
+def generate_data(n):
+    data = []
+    for i in range(n):
+        # generacja losowego tekstu
+        random_text = ' '.join(''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 8))) for _ in range(10))
+        data.append({"id": i, "text": random_text})
+    return data
+
+##########################################################
+# liczenie score
+def process_record(record):
+    text = record["text"].lower()
+    words = text.split()
+    unique_letters = len(set(c for c in text if c.isalpha()))
+    score = text.count('a') + text.count('e') + text.count('i') + text.count('o') + text.count('u') + text.count('y')
+    
+    return record["id"], { "word_count": len(words), "unique": unique_letters, "score": score }
+
+##########################################################
+###### SEKWENCYJNIE ######
+def run_sequential(data):
+    results = {}
+    start = time.time()
+    for record in data:
+        rid, res = process_record(record)
+        results[rid] = res
+    end = time.time()
+    return results, end - start
+
+##########################################################
+###### MULTIPROCESSING ######
+def chunk_worker(chunk):
+    # kazdy proces ma swoj wlasny lokalny slownik local_results
+    local_results = {}
+    for record in chunk:
+        rid, res = process_record(record)
+        local_results[rid] = res
+    return local_results
+
+def run_multiprocessing(data):
+    results = {}
+    start = time.time()
+    chunk_size = len(data) // NUM_WORKERS
+    chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+    
+    with multiprocessing.Pool(processes=NUM_WORKERS) as pool:
+        partial_results = pool.map(chunk_worker, chunks)
+        
+    # laczenie slownikow
+    for partial in partial_results:
+        results.update(partial)
+        
+    end = time.time()
+    return results, end - start
+
+##########################################################
+###### MULTITHREADING ######
+# Unsafe
+def run_threading_unsafe(data):
+    results = {}
+    num_threads = NUM_WORKERS
+    chunk_size = len(data) // num_threads
+    threads = []
+
+    def worker(chunk):
+        for record in chunk:
+            rid, res = process_record(record)
+            # modyfikacja wspolnego slownika
+            results[rid] = res
+
+    start = time.time()
+    for i in range(num_threads):
+        # podzial danych na porcje
+        start_idx = i * chunk_size
+        end_idx = None if i == num_threads - 1 else (i + 1) * chunk_size
+        chunk = data[start_idx:end_idx]
+        
+        t = threading.Thread(target=worker, args=(chunk,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+        
+    end = time.time()
+    return results, end - start
+
+# Thread-lock
+def run_threading_safe(data):
+    results = {}
+    lock = threading.Lock()
+    num_threads = NUM_WORKERS
+    chunk_size = len(data) // num_threads
+    threads = []
+
+    def worker(chunk):
+        for record in chunk:
+            rid, res = process_record(record)
+            # synchronizacja
+            with lock:
+                results[rid] = res
+
+    start = time.time()
+    for i in range(num_threads):
+        start_idx = i * chunk_size
+        end_idx = None if i == num_threads - 1 else (i + 1) * chunk_size
+        chunk = data[start_idx:end_idx]
+        
+        t = threading.Thread(target=worker, args=(chunk,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+        
+    end = time.time()
+    return results, end - start
+
+
+##########################################################
+##########################################################
+###### MAIN ######
+if __name__ == "__main__":
+    data = generate_data(NUM_RECORDS)
+
+    print(data[0])
+    print(process_record(data[0]))
+
+    ### Sekwencyjnie
+    results_0, time_0 = run_sequential(data)
+    print(f"Sekwencyjnie: {time_0:.4f}s")
+    
+    ### Multiprocessing
+    results_1, time_1 = run_multiprocessing(data)
+    is_correct_1 = True;
+    for i in range(NUM_RECORDS):
+        if ():
+    print(f"Multiprocessing: {time_1:.4f}s | Czy poprawny: {is_correct_1}")
+        
+    ### Threading
+    results_2, time_2 = run_threading_unsafe(data)
+    is_correct_2 = results_0 == results_2
+    print(f"Threading (bez zabezpieczen): {time_2:.4f}s | Czy poprawny: {is_correct_2}")
+        
+    ### Threading z synchronizacja
+    results_3, time_3 = run_threading_safe(data)
+    is_correct_3 = results_0 == results_3
+    print(f"Threading (z synchronizacja): {time_3:.4f}s | Czy poprawny: {is_correct_3}")
