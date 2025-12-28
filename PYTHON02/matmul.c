b87fa43f41e4de2f9be9ad5a48b089dce550742e
Jan Kwinta <jankwinta@icloud.com>
2025-12-28 16:06:38 +0100

    external C lib

---
+++ PYTHON02/matmul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void mult(int* A, int* B, int* C, int rowsA, int colsA, int colsB) {
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsB; j++) {
+            int sum = 0;
+            for (int k = 0; k < colsA; k++) {
+                sum += A[i * colsA + k] * B[k * colsB + j];
+            }
+            C[i * colsB + j] = sum;
+        }
+    }
+}
